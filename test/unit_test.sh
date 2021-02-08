9a5c363cd2e2b2ed2b2ec2b1140b4e3695e81a2d
E. Scott Daniels <daniels@research.att.com>
2021-02-08 16:13:49 -0500

    Added doc and unit test support
    The doc source to scrape changes.txt and geneate an RST
    file for the fluffy doc was added.
    
    Issue-ID: RIC-684
    
    Signed-off-by: E. Scott Daniels <daniels@research.att.com>
    Change-Id: Id6434bb56d1ddca16e343fd9caa94d6bddd9cd2f

---
+++ test/unit_test.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# vim: ts=4 sw=4 noet:
+#==================================================================================
+#       Copyright (c) 2020 Nokia
+#       Copyright (c) 2020 AT&T Intellectual Property.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#==================================================================================
+
+#
+#	Mnemonic:	unit_test.sh
+#	Abstract:	For now there are no unit tests (the code here is all geneated).
+#				Should it become necessary to have tests, this scirpt will be
+#				modified to drive them.
+#
+#	Date:		08 February 2021
+#	Author:		E. Scott Daniels
+# -----------------------------------------------------------------------------
+
+
+echo "[INFO] unit tests completed successfully"
+exit 0
+
